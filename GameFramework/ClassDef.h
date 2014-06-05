ca379db2ad0ef79c197ba9c1c3eea5286f957e5a
DrCosinus <minwene@free.fr>
2014-06-05 22:37:24 +0200

    GFW reflection

--- GameFramework/ClassDef.h
+++ GameFramework/ClassDef.h
@@ -4,254 +4,289 @@
 #include <string>
 #include <vector>
 
-class Value;
-
-class BaseValue abstract
+namespace RFX
 {
-public: 
-    virtual ~BaseValue()
-    {} 
-    virtual BaseValue* clone() const = 0; 
-    virtual std::string asString() const = 0; // fromString() 
-    virtual void set(const Value& v) = 0; // no get()! 
-private: 
-    // Type info 
-};
+    class Value;
 
-
-
-template <typename PlainT> 
-class RealValue : public BaseValue 
-{
-public: 
-    RealValue(PlainT v) : val(v) 
-    {} 
-    RealValue* clone() const 
-    { 
-        return new RealValue(*this); 
-    } 
-    std::string asString() const override 
-    { 
-        std::ostringstream os; 
-        os << val; 
-        return os.str(); 
-    } 
-    operator PlainT() const // conversion to plain type 
-    { 
-        return val; 
-    } 
-    void RealValue<PlainT>::set(Value const & v) override
-    { 
-        val = v.get<PlainT>(); 
-    } 
-private: 
-    PlainT val; 
-};
-
-
-
-
-
-class Value // Value handle 
-{ 
-public: 
-    Value(const BaseValue& bv) : v(bv.clone())
-    {} 
-    Value(const Value& rhs) : v(rhs.v ? rhs.v->clone() : nullptr)
-    {}
-    explicit Value(BaseValue* bv = nullptr) : v(bv) 
-    {} 
-    ~Value() 
+    class BaseValue abstract
     {
-        if (v)
+    public:
+        virtual ~BaseValue()
+        {}
+        virtual BaseValue* clone() const = 0;
+        virtual std::string asString() const = 0; // fromString() 
+        virtual void set(const Value& v) = 0; // no get()! 
+    private:
+        // Type info 
+    };
+
+
+
+    template <typename PlainT>
+    class RealValue : public BaseValue
+    {
+    public:
+        RealValue(PlainT v) : val(v)
+        {}
+        RealValue* clone() const
         {
-            delete v;
-            v = nullptr;
+            return new RealValue(*this);
         }
-    } 
-    Value& operator=(const Value& rhs) 
-    { 
-        // this is not a typical pimpl assignment, but a set() 
-        if (v) 
-        { 
-            if (rhs.v) 
-            { 
-                v->set(rhs);
-            } 
-            else 
+        std::string asString() const override
+        {
+            std::ostringstream os;
+            os << val;
+            return os.str();
+        }
+        operator PlainT() const // conversion to plain type 
+        {
+            return val;
+        }
+        void RealValue<PlainT>::set(Value const & v) override
+        {
+            val = v.get<PlainT>();
+        }
+    private:
+        PlainT val;
+    };
+
+
+
+    class Value // Value handle 
+    {
+    public:
+        Value(const BaseValue& bv) : v(bv.clone())
+        {}
+        Value(const Value& rhs) : v(rhs.v ? rhs.v->clone() : nullptr)
+        {}
+        explicit Value(BaseValue* bv = nullptr) : v(bv)
+        {}
+        ~Value()
+        {
+            if (v)
             {
                 delete v;
                 v = nullptr;
-            } 
-        } 
-        else 
-        { 
-            v = (rhs.v ? rhs.v->clone() : nullptr); 
-        } 
-        return *this; 
-    } 
+            }
+        }
+        Value& operator=(const Value& rhs)
+        {
+            // this is not a typical pimpl assignment, but a set() 
+            if (v)
+            {
+                if (rhs.v)
+                {
+                    v->set(rhs);
+                }
+                else
+                {
+                    delete v;
+                    v = nullptr;
+                }
+            }
+            else
+            {
+                v = (rhs.v ? rhs.v->clone() : nullptr);
+            }
+            return *this;
+        }
 
-    template <typename PlainT> 
-    PlainT get() const 
-    { 
-        if (v) 
-        { 
-            const RealValue<PlainT>& rv = dynamic_cast<const RealValue<PlainT>&>(*v);
-            return rv; 
-        } 
-        else 
-        { 
-            return PlainT(); 
-        } 
-    } 
-    std::string asString() const 
-    { 
-        if (v) 
-        { 
-            return v->asString(); 
-        } 
-        else 
-        { 
-            return std::string(); 
-        } 
-    } 
-private: 
-    BaseValue*  v; 
-}; 
+        template <typename PlainT>
+        PlainT get() const
+        {
+            if (v)
+            {
+                const RealValue<PlainT>& rv = dynamic_cast<const RealValue<PlainT>&>(*v);
+                return rv;
+            }
+            else
+            {
+                return PlainT();
+            }
+        }
+        std::string asString() const
+        {
+            if (v)
+            {
+                return v->asString();
+            }
+            else
+            {
+                return std::string();
+            }
+        }
+    private:
+        BaseValue*  v;
+    };
 
 
 
-
-
-
-
-
-class Type 
-{ 
-public: 
-    enum TypeT 
-    { 
-        stringT, 
-        intT, 
-        doubleT, 
-        unknownT 
-    }; 
-    explicit Type(TypeT typeId_) : typeId(typeId_) 
+    class Type
     {
-    } 
-    BaseValue* newValue() const 
-    { 
-        return prototypes[typeId]->clone(); 
-    } 
-    TypeT getType() const 
-    { 
-        return typeId; 
-    } 
-    static void init() 
-    { 
-        prototypes[stringT] = new RealValue<std::string>(""); 
-        prototypes[intT] = new RealValue<int>(0);
-        prototypes[doubleT] = new RealValue<double>(0);
-    } 
-private: 
-    TypeT typeId; 
-    static std::vector<BaseValue*> prototypes; 
-}; 
-std::vector<BaseValue*> Type::prototypes(Type::unknownT);
+    public:
+        enum TypeT
+        {
+            stringT,
+            intT,
+            doubleT,
+            unknownT
+        };
+        explicit Type(TypeT typeId_) : typeId(typeId_)
+        {
+        }
+        BaseValue* newValue() const
+        {
+            return prototypes[typeId]->clone();
+        }
+        TypeT getType() const
+        {
+            return typeId;
+        }
+        static void init()
+        {
+            prototypes[stringT] = new RealValue<std::string>("");
+            prototypes[intT] = new RealValue<int>(0);
+            prototypes[doubleT] = new RealValue<double>(0);
+        }
+    private:
+        TypeT typeId;
+        static std::vector<BaseValue*> prototypes;
+    };
+    std::vector<BaseValue*> Type::prototypes(Type::unknownT);
 
 
 
-
-
-
-
-class Attribute 
-{ 
-public: 
-    Attribute(const std::string & name_, Type::TypeT typeId) : name(name_), type_(typeId) 
+    class Attribute
     {
-    } 
-    const std::string & getName() const 
-    { 
-        return name; 
-    } 
-    Type getType() const
-    { 
-        return type_; 
-    } 
-private: 
-    std::string name; 
-    Type type_; 
-};
+    public:
+        Attribute(const std::string & name_, Type::TypeT typeId) : name(name_), type_(typeId)
+        {
+        }
+        const std::string & getName() const
+        {
+            return name;
+        }
+        Type getType() const
+        {
+            return type_;
+        }
+    private:
+        std::string name;
+        Type type_;
+    };
 
 
+    class Object;
 
-
-
-class ClassDef;
-
-class Object
-{
-public:
-    Object(const ClassDef*);
-};
-
-class ClassDef
-{
-    //typedefs Container, Iterator for attributes 
-public:
-    ClassDef(const ClassDef* base, const std::string& name_)
-        : baseClass(base)
-        , name(name_)
-        , definitionFix(false)
+    class ClassDef
     {
-        baseInit();
-        effectiveAttributes.insert(effectiveAttributes.end(), ownAttributes.begin(), ownAttributes.end());
-    }
-    template <typename iterator>
-    ClassDef(const ClassDef* base, const std::string& name_, iterator attribBegin, iterator attribEnd)
-        : baseClass(base), name(name_)
-        , ownAttributes(attribBegin, attribEnd)
-        , definitionFix(false)
+        friend class Object;
+        //typedefs Container, Iterator for attributes 
+        typedef std::vector<Attribute> AttributeContainer;
+        typedef AttributeContainer::iterator AttrIterator;
+    public:
+        ClassDef(const ClassDef* base, const std::string& name_)
+            : baseClass(base)
+            , name(name_)
+            , definitionFix(false)
+        {
+            baseInit();
+            effectiveAttributes.insert(effectiveAttributes.end(), ownAttributes.begin(), ownAttributes.end());
+        }
+        template <typename iterator>
+        ClassDef(const ClassDef* base, const std::string& name_, iterator attribBegin, iterator attribEnd)
+            : baseClass(base), name(name_)
+            , ownAttributes(attribBegin, attribEnd)
+            , definitionFix(false)
+        {
+            baseInit();
+            effectiveAttributes.insert(effectiveAttributes.end(), ownAttributes.begin(), ownAttributes.end());
+        }
+        std::string getName() const;
+        Object* newObject() const;
+        AttrIterator attribBegin() const;
+        AttrIterator attribEnd() const;
+        const Attribute& getAttribute(size_t idx) const;
+        void addAttribute(const Attribute&);
+        size_t getAttributeCount() const;
+        size_t findAttribute(const std::string& name) const
+        {
+            // this does a reverse search to find the most derived 
+            AttributeContainer::const_reverse_iterator i;
+            for (i = effectiveAttributes.rbegin(); i != effectiveAttributes.rend(); ++i)
+            {
+                if (i->getName() == name)
+                {
+                    return std::distance(i, effectiveAttributes.rend()) - 1;
+                }
+            }
+            return getAttributeCount();
+        }
+    private:
+        void baseInit()
+        {
+            if (baseClass)
+            {
+                baseClass->definitionFix = true;
+                std::copy(baseClass->attribBegin(), baseClass->attribEnd(), std::back_inserter<AttributeContainer>(effectiveAttributes));
+            }
+        }
+        const ClassDef* const baseClass;
+        std::string name;
+        AttributeContainer ownAttributes, effectiveAttributes;
+        mutable bool definitionFix;
+    };
+
+
+
+    class Object
     {
-        baseInit();
-        effectiveAttributes.insert(effectiveAttributes.end(), ownAttributes.begin(), ownAttributes.end());
-    }
-    std::string getName() const;
-    Object* newObject() const
+    public:
+        explicit Object(const ClassDef* class_) : myClass(class_), values(class_->getAttributeCount())
+        {
+            buildValueList();
+        }
+        const ClassDef& instanceOf() const
+        {
+            return *myClass;
+        }
+        Value getValue(size_t attribIdx) const
+        {
+            return *values[attribIdx]; // calls Value(BaseValue &) 
+        }
+        Value getValue(std::string const &attribName) const
+        {
+            size_t idx = instanceOf().findAttribute(attribName); // should check for not found 
+            return getValue(idx);
+        }
+        void setValue(size_t idx, Value const& v)
+        {
+            values[idx]->set(v);
+        }
+        void setValue(std::string const& attribName, Value const &v)
+        {
+            size_t idx = instanceOf().findAttribute(attribName); // should check for not found 
+            setValue(idx, v);
+        }
+    private:
+        typedef std::vector<BaseValue*> ValueContainer;
+        void buildValueList()
+        {
+            ClassDef::AttrIterator a;
+            ValueContainer::iterator i = values.begin();
+            for (a = instanceOf().attribBegin(); a != instanceOf().attribEnd(); ++a, ++i)
+            {
+                *i = a->getType().newValue();
+            }
+        }
+        ClassDef const * const myClass;
+        ValueContainer values;
+    };
+
+
+    Object* ClassDef::newObject() const
     {
         definitionFix = true;
         return new Object(this);
     }
-    AttrIterator attribBegin() const;
-    AttrIterator attribEnd() const;
-    const Attribute& getAttribute(size_t idx) const;
-    void addAttribute(const Attribute&);
-    size_t getAttributeCount() const;
-    size_t findAttribute(const std::string& name) const
-    {
-        // this does a reverse search to find the most derived 
-        AttributeContainer::const_reverse_iterator i;
-        for (i = effectiveAttributes.rbegin(); i != effectiveAttributes.rend(); ++i)
-        {
-            if (i - getName() == name)
-            {
-                return std::distance(i, effectiveAttributes.rend()) - 1;
-            }
-        }
-        return getAttributeCount();
-    }
-private:
-    void baseInit()
-    {
-        if (baseClass)
-        {
-            baseClass - definitionFix = true;
-            copy(baseClass->attribBegin(), baseClass->attribEnd(), back_inserter<AttributeContainer>(effectiveAttributes));
-        }
-    }
-    const ClassDef* const baseClass;
-    std::string name;
-    AttributeContainer ownAttributes, effectiveAttributes;
-    mutable bool definitionFix;
-};
+}
--- GameFramework/Model.cpp
+++ GameFramework/Model.cpp
@@ -12,7 +12,7 @@
 
 
 // insparation: http://www.enchantedage.com/cpp-reflection
-
+/*
 struct TypeBase
 {
     virtual void Marshal(void*, const void*) const = 0;
@@ -93,10 +93,10 @@ void ReflectionBase::ReflectionConstruct()
             return instance<_info>(); \
         } \
     }; \
-    static const member_t* members() { return _info/*<_type>*/::info().members(); } \
-    static _info/*<_type>*/ _theInfo; \
-    static inline const _info/*<_type>*/& info() { return _theInfo; }
-
+    static const member_t* members() { return _info::info().members(); } \
+    static _info _theInfo; \
+    static inline const _info& info() { return _theInfo; }
+    */
 #define DECLARE_MODEL(_MODEL_CLASSNAME_, _MODEL_SUPERCLASSNAME_) \
 public: \
     typedef _MODEL_SUPERCLASSNAME_ Super; \
@@ -162,10 +162,10 @@ namespace Model
     class Light : public Model
     {
         DECLARE_MODEL(Light, Model)
-        DECLARE_MODEL_VARIABLE(float, myIntensity);
+        //DECLARE_MODEL_VARIABLE(float, myIntensity);
 
-        RTTI(Light, MEMBER(myIntensity));
-
+        //RTTI(Light, MEMBER(myIntensity));
+        float myIntensity;
     public:
         Light(const protected_tag& aProtectedTag, std::string&& aName, float anIntensity) : Model(aProtectedTag, std::forward<std::string>(aName)), myIntensity(anIntensity)
         {
@@ -176,7 +176,9 @@ namespace Model
     class OmniLight : public Light
     {
         DECLARE_MODEL(OmniLight, Light)
-        DECLARE_MODEL_VARIABLE(float, myRadius);
+        //DECLARE_MODEL_VARIABLE(float, myRadius);
+
+        float myRadius;
     public:
         OmniLight(const protected_tag& aProtectedTag, std::string&& aName, float anIntensity, float aRadius) : Light(aProtectedTag, std::forward<std::string>(aName), anIntensity), myRadius(aRadius)
         {
@@ -194,10 +196,10 @@ namespace Model
     };
     IMPLEMENT_MODEL(Renderer)
 
-    Light::_info Light::_theInfo;
+    //Light::_info Light::_theInfo;
 }
-template<typename T>
-typename T::_info T::_theInfo;
+//template<typename T>
+//typename T::_info T::_theInfo;
 
 
 //#define PP_NARG( ...) PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
@@ -205,26 +207,26 @@ typename T::_info T::_theInfo;
 //#define PP_ARG_N(_1,_2,_3,_4,_5,_6,_7,_8,_9,[..],_61,_62,_63,N,...) N
 //#define PP_RSEQ_N() 63,62,61,60,[..],9,8,7,6,5,4,3,2,1,0
 
-template<typename T>
-struct function_traits;
-
-template<typename R, typename... Ts>
-struct function_traits<R(Ts...)>
-{
-    using return_type = R;
-    static const std::size_t arity = sizeof...(Ts);
-
-    template <std::size_t N>
-    struct argument
-    {
-        static_assert(N < arity, "invalid parameter index.");
-        using type = typename std::tuple_element<N, std::tuple<Ts...>>::type;
-    };
-};
+//template<typename T>
+//struct function_traits;
+//
+//template<typename R, typename... Ts>
+//struct function_traits<R(Ts...)>
+//{
+//    using return_type = R;
+//    static const std::size_t arity = sizeof...(Ts);
+//
+//    template <std::size_t N>
+//    struct argument
+//    {
+//        static_assert(N < arity, "invalid parameter index.");
+//        using type = typename std::tuple_element<N, std::tuple<Ts...>>::type;
+//    };
+//};
 
 //#define SIXTYFOURTH(_1,_2,_3,_4_,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,_21,_22,_23,_24,_25,_26,_27,_28,_29,_30,_31,_32,_33,_34,_35,_36,_37,_38,_39,_40,_41,_42,_43,_44,_45,_46,_47,_48,_49,_50,_51,_52,_53,_54,_55,_56,_57,_58,_59,_60,_61,_62,_63, N,...) N
 //#define SIZEOF_VA_ARG(...) SIXTYFOURTH(__VA_ARGS__,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
-#define REFLECTABLE(...) function_traits<void(__VA_ARGS__)>::arity
+//#define REFLECTABLE(...) function_traits<void(__VA_ARGS__)>::arity
 
 
 //constexpr char* plop() { return "plop"; }
@@ -232,38 +234,38 @@ struct function_traits<R(Ts...)>
 //constexpr std::size_t plop() { return function_traits<zozo /*void(*)(float toto, float tata, int titi)*/>::arity; }
 
 //constexpr std::size_t plop() { return function_traits<void(float toto, float tata, int titi, char* yoyo)>::arity; }
+//
+//template<typename T>
+//constexpr const auto& name();
+//
+//template<>
+//constexpr const auto& name<Model::Light>()
+//{
+//    return "Light";
+//}
 
-template<typename T>
-constexpr const auto& name();
-
-template<>
-constexpr const auto& name<Model::Light>()
-{
-    return "Light";
-}
 
 
-
-constexpr int c_strcmp(const char* l, const char* r)
-{
-    return (*l == *r) ? (*l == '\0' ? 0 : c_strcmp(l + 1, r + 1)) : *l - *r;
-}
-typedef std::tuple<Model::OmniLight, Model::Light, Model::Renderer> ModelTypes;
+//constexpr int c_strcmp(const char* l, const char* r)
+//{
+//    return (*l == *r) ? (*l == '\0' ? 0 : c_strcmp(l + 1, r + 1)) : *l - *r;
+//}
+//typedef std::tuple<Model::OmniLight, Model::Light, Model::Renderer> ModelTypes;
 
 void Test()
 {
 
-    std::cout
-        << "Debug: " << function_traits<void(float toto, float tata/*, int titi, char* yoyo*/)>::arity
-        << ",zuzu: " << name<Model::Light>()
-        << " --- " << c_strcmp("Light", "Lumiere")
-        << std::endl;
+    //std::cout
+    //    << "Debug: " << function_traits<void(float toto, float tata/*, int titi, char* yoyo*/)>::arity
+    //    << ",zuzu: " << name<Model::Light>()
+    //    << " --- " << c_strcmp("Light", "Lumiere")
+    //    << std::endl;
 
-    std::cout
-        << Model::Light::info().name()
-        << " "
-        << Model::Light::info().size
-        << std::endl;
+    //std::cout
+    //    << Model::Light::info().name()
+    //    << " "
+    //    << Model::Light::info().size
+    //    << std::endl;
 
 
     auto l = Model::Light::Create(5.0f);
